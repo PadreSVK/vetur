6e2d8ba67604eb681f71fe5b712906fcbd2a6aea
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-06-21 22:18:48 +0200

    add sample for bad formating

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="page">
     <counter></counter>
+    <v-data-table
+      :headers="headers"
+      :search="search"
+      :items="balanceTreeData"
+      :loading="loading"
+      :items-per-page="Number(limitValue) || 0"
+      dense
+      class="border"
+      hide-default-footer
+    >
+      <template
+        v-slot:[`header.${h.value}`]="{ header }"
+        v-for="h in headers">
+        <v-tooltip
+          :key="h.text"
+          bottom>
+          <template v-slot:activator="{ on }">
+            <span v-on="on">{{ h.text }}</span>
+          </template>
+          <span>{{ h.tooltip }}</span>
+        </v-tooltip>
+      </template>
+    </v-data-table>
+
   </div>
 </template>
 
